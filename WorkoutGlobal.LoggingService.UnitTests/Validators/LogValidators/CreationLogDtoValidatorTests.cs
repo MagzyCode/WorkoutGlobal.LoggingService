cd3a3159d679d403172c6f8a419bedbaa5837b17
MagzyCode <mihailkazarevich@mail.ru>
2022-11-10 18:15:36 +0300

    Create unit-tests for validators.

---
+++ WorkoutGlobal.LoggingService.UnitTests/Validators/LogValidators/CreationLogDtoValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentValidation.Results;
+using System.Security.Cryptography;
+using WorkoutGlobal.LoggingService.Api.Dto;
+using WorkoutGlobal.LoggingService.Api.Validators;
+
+namespace WorkoutGlobal.LoggingService.UnitTests.Validators
+{
+    public class CreationLogDtoValidatorTests
+    {
+        public CreationLogDtoValidator Validator { get; set; } = new();
+
+        public Fixture Fixture { get; set; } = new();
+
+        [Fact]
+        public async Task ModelState_NullProperties_ReturnValidationResult()
+        {
+            // arrange
+            var creationLogDto = new CreationLogDto();
+
+            // act
+            var validationResult = await Validator.ValidateAsync(creationLogDto);
+
+            // assert
+            validationResult.Should().BeOfType(typeof(ValidationResult));
+            validationResult.Should().NotBeNull();
+            validationResult.Errors.Should().HaveCount(2);
+            validationResult.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ModelState_EmptyProperties_ReturnValidationResult()
+        {
+            // arrange
+            var creationLogDto = Fixture.Build<CreationLogDto>()
+                .With(x => x.Message, string.Empty)
+                .With(x => x.Severity, string.Empty)
+                .Create();
+
+            // act
+            var validationResult = await Validator.ValidateAsync(creationLogDto);
+
+            // assert
+            validationResult.Should().BeOfType(typeof(ValidationResult));
+            validationResult.Should().NotBeNull();
+            validationResult.Errors.Should().HaveCount(2);
+            validationResult.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ModelState_ValidProperties_ReturnValidationResult()
+        {
+            // arrange
+            var creationLogDto = Fixture.Create<CreationLogDto>();
+
+            // act
+            var validationResult = await Validator.ValidateAsync(creationLogDto);
+
+            // assert
+            validationResult.Should().BeOfType(typeof(ValidationResult));
+            validationResult.Should().NotBeNull();
+            validationResult.Errors.Should().HaveCount(0);
+            validationResult.IsValid.Should().BeTrue();
+        }
+    }
+}
---
+++ WorkoutGlobal.LoggingService.UnitTests/Validators/SeverityValidators/CreationSeverityDtoValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutGlobal.LoggingService.Api.Dto;
+using WorkoutGlobal.LoggingService.Api.Validators;
+
+namespace WorkoutGlobal.LoggingService.UnitTests.Validators.SeverityValidators
+{
+    public class CreationSeverityDtoValidatorTests
+    {
+        public CreationSeverityDtoValidator Validator { get; set; } = new();
+
+        public Fixture Fixture { get; set; } = new();
+
+        [Fact]
+        public async Task ModelState_NullProperties_ReturnValidationResult()
+        {
+            // arrange
+            var creationSeverityDto = new CreationSeverityDto();
+
+            // act
+            var validationResult = await Validator.ValidateAsync(creationSeverityDto);
+
+            // assert
+            validationResult.Should().BeOfType(typeof(ValidationResult));
+            validationResult.Should().NotBeNull();
+            validationResult.Errors.Should().HaveCount(2);
+            validationResult.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ModelState_EmptyProperties_ReturnValidationResult()
+        {
+            // arrange
+            var creationSeverityDto = Fixture.Build<CreationSeverityDto>()
+                .With(x => x.SeverityName, string.Empty)
+                .With(x => x.SeveriryDescription, string.Empty)
+                .Create();
+
+            // act
+            var validationResult = await Validator.ValidateAsync(creationSeverityDto);
+
+            // assert
+            validationResult.Should().BeOfType(typeof(ValidationResult));
+            validationResult.Should().NotBeNull();
+            validationResult.Errors.Should().HaveCount(2);
+            validationResult.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task ModelState_ValidProperties_ReturnValidationResult()
+        {
+            // arrange
+            var creationSeverityDto = Fixture.Create<CreationSeverityDto>();
+
+            // act
+            var validationResult = await Validator.ValidateAsync(creationSeverityDto);
+
+            // assert
+            validationResult.Should().BeOfType(typeof(ValidationResult));
+            validationResult.Should().NotBeNull();
+            validationResult.Errors.Should().HaveCount(0);
+            validationResult.IsValid.Should().BeTrue();
+        }
+    }
+}
